031f463a132991aafbef973e72218e4591f70ce8
Joseph Manangan <jmanangan@3cloudsolutions.com>
2024-01-24 04:09:26 +0800

    Add project files.

---
+++ AccessingPrivateVariables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+public class MyClass
+{
+    private int privateVariable = 42;
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Inside private method");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass myObject = new MyClass();
+
+        // Accessing private variable using reflection
+        FieldInfo privateVariableInfo = typeof(MyClass).GetField("privateVariable", BindingFlags.NonPublic | BindingFlags.Instance);
+        int value = (int)privateVariableInfo.GetValue(myObject);
+        Console.WriteLine($"Private Variable Value: {value}");
+
+        // Accessing private method using reflection
+        MethodInfo privateMethodInfo = typeof(MyClass).GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+        privateMethodInfo.Invoke(myObject, null);
+
+        Console.ReadLine();
+    }
+}
